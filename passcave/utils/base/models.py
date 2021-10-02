cb4b1d4230b79ce3008d8a6fa545ed6f29094814
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-10-03 01:04:31 +0530

    chore: base model update

--- passcave/utils/base/models.py
+++ passcave/utils/base/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import uuid
 
 class BaseModel(models.Model):
     id = models.UUIDField(
-        primary_key=True
+        primary_key=True,
+         default = uuid.uuid4,
+         editable = False
     )
     created_at = models.DateTimeField(
         auto_now_add=True
