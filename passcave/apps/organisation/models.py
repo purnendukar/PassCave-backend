5cba91f44c1653a3c5e4389d4d48b5f002392af0
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-10-03 16:51:22 +0530

    feat: organisation app

---
+++ passcave/apps/organisation/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from utils.base.models import BaseModel
+from apps.user.models import User
+
+# Create your models here.
+class Organisation(BaseModel):
+    ORG_TYPE_CHOICES = [
+        (1, 'Family'),
+        (2, 'Organisation')
+    ]
+    org_type = models.TextField(
+        choices=ORG_TYPE_CHOICES,
+        null=True,
+        blank=True,
+        default=2
+    )
+    admin = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="owned_organisation",
+        null=True,
+        blank=True
+    )
+    members = models.ManyToManyField(
+        to=User,
+        related_name="organisations"
+    )
+
