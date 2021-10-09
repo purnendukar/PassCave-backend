435289989cbe1b75022445a204422805b5bf1759
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-10-09 19:30:55 +0530

    feat: base

---
+++ passcave/apps/base/mixins.py
@@ -0,0 +1,20 @@
+class MultiSerializerViewMixin:
+    serializer_classes = {}
+    
+    def get_serializer_class(self):
+        if self.actions in self.serializer_classes:
+            return self.serializer_classes[self.actions]
+        return super().get_serializer_class()
+
+
+class MultiRequestValidatorViewMixin:
+    request_serializer_classes = {}
+
+    def request_valiator(self):
+        serializer = self.request_serializer_classes[self.actions](self.request.data)
+        return (serializer.validated_data, serializer.context)
+
+
+
+
+
