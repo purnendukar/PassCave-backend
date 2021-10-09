a8ec31664c04de61d5ff0094bd9831a2cd10dc0f
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-10-09 19:32:09 +0530

    feat: token authentication drf

--- passcave/passcave/settings.py
+++ passcave/passcave/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework.authtoken',
+
     'apps.user',
     'apps.plan',
     'apps.credential',
