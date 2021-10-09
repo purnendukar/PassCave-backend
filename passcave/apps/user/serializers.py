de7ed7f5b438d38b0792a69695115987da77c61a
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-10-09 19:33:05 +0530

    feat: auth viewset and serializer

---
+++ passcave/apps/user/serializers.py
@@ -0,0 +1,49 @@
+from django.db.models import fields
+from rest_framework import serializers
+
+from rest_framework.authtoken.models import Token
+
+from apps.user.models import User, UserProfile
+
+
+class AuthRequestSerializers(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+
+    class Meta:
+        fields = ["email", "password"]
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
+
+
+class UserAuthSerializers(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField()
+    profile = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "profile",
+            "token"
+        ]
+    
+    def token(self, user):
+        token,created = Token.objects.get_or_create(user=user)
+        return token
+
+    def profile(self, instance):
+        if hasattr(instance, "profile"):
+            return UserProfileSerializer(
+                instance.profile
+            ).data
+        return None
+    
+
+
+
+
--- passcave/apps/user/views.py
+++ passcave/apps/user/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from apps.base import mixins
+
+from apps.user.models import User
+from apps.user.serializers import AuthRequestSerializers, UserAuthSerializers
+
 
 # Create your views here.
+class AuthViewMixin(mixins.MultiRequestValidatorViewMixin, mixins.MultiSerializerViewMixin):
+    pass
+
+
+class AuthViewset(mixins.MultiRequestValidatorViewMixin, viewsets.GenericViewSet):
+    model = User
+    queryset = model.objects.all()
+    # serializer_classes = {
+    #     "register": AuthRequestSerializers
+    # }
+    serializer_class = UserAuthSerializers
+    request_serializer_classes = {
+        "register": AuthRequestSerializers,
+        "login": AuthRequestSerializers
+    }
+
+    @action(methods=["POST"], permission_classes=[])
+    def register(self, request):
+        data,context = self.request_valiator()
+        user = self.model.create_user(**data)
+        serializer = self.get_serializer(
+            user,
+            context
+        )
+        return Response(
+            {
+                "message": "Registration successful",
+                "data": serializer.data
+            },
+            status.HTTP_201_CREATED
+        )
+
+    @action(methods=["POST"], permission_classes=[])
+    def login(self, request):
+        data,context = self.request_valiator()
+        user = authenticate(**data)
+        serializer = self.get_serializer(
+            user,
+            context
+        )
+        return Response(
+            {
+                "message": "Login successful",
+                "data": serializer.data
+            }
+        )
+        
+
+    @action(methods=["POST"], permission_classes=[])
+    def logout(self, request):
+        request.auth.delete()
+        return Response(
+            {
+                "message": "Logout Succcessful"
+            }
+        )
+        
+
+
