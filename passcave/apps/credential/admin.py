a08d0d192b570d998c624a508c28a3b08502206f
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 02:47:19 +0530

    feat: credential model admin

--- passcave/apps/credential/admin.py
+++ passcave/apps/credential/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from apps.credential.models import Credential
 
-# Register your models here.
-class CredentialAdmin(admin.ModelAdmin):
-    readonly_fields = ("detail",)
+from apps.credential.models import (
+    BankCard,
+    BankDetail,
+    WebApplication,
+    UPIGateway,
+    SecretNote,
+    Identity,
+)
 
 
-admin.site.register(Credential, CredentialAdmin)
+class BaseAdmin(admin.ModelAdmin):
+    filter_horizontal = ("access_given",)
+
+
+admin.site.register(BankCard, BaseAdmin)
+admin.site.register(BankDetail, BaseAdmin)
+admin.site.register(WebApplication, BaseAdmin)
+admin.site.register(UPIGateway, BaseAdmin)
+admin.site.register(SecretNote, BaseAdmin)
+admin.site.register(Identity, BaseAdmin)
