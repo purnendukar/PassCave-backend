2a5a65b2f33763b9f714b9b147d120d75a104855
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 02:47:46 +0530

    feat: credential model serializer

--- passcave/apps/credential/serializers.py
+++ passcave/apps/credential/serializers.py
@@ -1,11 +1,50 @@
 from django.db.models import fields
 from rest_framework import serializers
 
-from apps.credential.models import Credential
+from apps.credential.models import (
+    BankCard,
+    BankDetail,
+    WebApplication,
+    UPIGateway,
+    SecretNote,
+    Identity,
+)
+from apps.user.serializers import UserSerializers
 
 
-class CredentialSerializer(serializers.ModelSerializer):
+class BaseSerializer(serializers.Serializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["owned_by"] = UserSerializers(instance.owned_by).data
+        data["access_given"] = UserSerializers(instance.access_given.all()).data
+        return data
+
+
+class BankCardSerializer(BaseSerializer, serializers.ModelSerializer):
     class Meta:
-        model = Credential
-        fields = ["id", "cred_type", "owned_by", "owned_by", "access_given", "detail"]
+        model = BankCard
 
+
+class BankDetailSerializer(BaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = BankDetail
+
+
+class WebApplicationSerializer(BaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = WebApplication
+
+
+class UPIGatewayerializer(BaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = UPIGateway
+
+
+class SecretNoteSerializer(BaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = SecretNote
+
+
+class IdentitySerializer(BaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = Identity
