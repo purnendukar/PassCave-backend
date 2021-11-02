68046aab030fb99b17eff4ec824948fe57acdb75
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 02:48:16 +0530

    feat: bank card api viewset

--- passcave/apps/credential/apis.py
+++ passcave/apps/credential/apis.py
@@ -1,9 +1,23 @@
 from rest_framework import viewsets
 
-from apps.credential.models import Credential
-from apps.credential.serializers import CredentialSerializer
+from apps.credential.models import (
+    BankCard,
+    BankDetail,
+    WebApplication,
+    UPIGateway,
+    SecretNote,
+    Identity,
+)
+from apps.credential.serializers import (
+    BankCardSerializer,
+    BankDetailSerializer,
+    WebApplicationSerializer,
+    UPIGatewayerializer,
+    SecretNoteSerializer,
+    IdentitySerializer,
+)
 
 
-class CredentialViewset(viewsets.ModelViewSet):
-    queryset = Credential
-    serializer_class = CredentialSerializer
+class BankCardViewSet(viewsets.ModelViewSet):
+    queryset = BankCard.objects.all()
+    serializer_class = BankCardSerializer
