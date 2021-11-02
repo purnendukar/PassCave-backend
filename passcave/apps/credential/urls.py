bc27a743f4ab63dc81bf92255562694cc47ccf1d
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 03:07:41 +0530

    chore: format

--- passcave/apps/credential/urls.py
+++ passcave/apps/credential/urls.py
@@ -1,7 +1,14 @@
 from rest_framework import urlpatterns
 from rest_framework.routers import DefaultRouter
 
-from apps.credential.apis import BankCardViewSet, BankDetailViewSet, SecretNoteViewSet, UPIGatewayViewSet, IdentityViewSet, WebApplicationViewSet
+from apps.credential.apis import (
+    BankCardViewSet,
+    BankDetailViewSet,
+    SecretNoteViewSet,
+    UPIGatewayViewSet,
+    IdentityViewSet,
+    WebApplicationViewSet,
+)
 
 
 default_router = DefaultRouter(trailing_slash=False)
@@ -10,7 +17,9 @@ default_router.register("bank_card", BankCardViewSet, basename="bank_card")
 default_router.register("bank_detail", BankDetailViewSet, basename="bank_detail")
 default_router.register("upi_gateway", UPIGatewayViewSet, basename="upi_gateway")
 default_router.register("identity", IdentityViewSet, basename="identity")
-default_router.register("web_application", WebApplicationViewSet, basename="web_application")
+default_router.register(
+    "web_application", WebApplicationViewSet, basename="web_application"
+)
 default_router.register("secret_note", SecretNoteViewSet, basename="secret_note")
 
 
