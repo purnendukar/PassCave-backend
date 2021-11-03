7b9ebbb1cd34686f955497dacf91905a8c3d5fcf
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 13:55:54 +0530

    feat: Plan Admin

--- passcave/apps/plan/admin.py
+++ passcave/apps/plan/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from apps.plan.models import Plan
+
+
 # Register your models here.
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "amount", "members_count")
+
+
+admin.site.register(Plan, PlanAdmin)
