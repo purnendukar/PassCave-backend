7f23363a4869b81cfdcf4ec46ad9fe20faea80e3
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 15:44:49 +0530

    feat: auth api url

--- passcave/passcave/api_urls.py
+++ passcave/passcave/api_urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from django.urls.conf import include
 
+from rest_framework.routers import DefaultRouter
+
+from apps.user.apis import AuthViewset
+
+
+default_router = DefaultRouter(trailing_slash=False)
+
+default_router.register("auth", AuthViewset, basename="auth")
+
 
 urlpatterns = [path("credential/", include("apps.credential.urls"), name="credential")]
+urlpatterns += default_router.urls
