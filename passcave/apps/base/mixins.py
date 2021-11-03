b9aae4ef6e66415a7579a657d28597dbfb1ecfe5
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 15:43:18 +0530

    fix: base mixins

--- passcave/apps/base/mixins.py
+++ passcave/apps/base/mixins.py
@@ -2,8 +2,8 @@ class MultiSerializerMixin:
     serializer_classes = {}
 
     def get_serializer_class(self):
-        if self.actions in self.serializer_classes:
-            return self.serializer_classes[self.actions]
+        if self.action in self.serializer_classes:
+            return self.serializer_classes[self.action]
         return super().get_serializer_class()
 
 
@@ -11,8 +11,9 @@ class MultiRequestValidatorMixin:
     request_serializer_classes = {}
 
     def request_valiator(self):
-        if self.actions in self.request_serializer_classes:
-            serializer = self.request_serializer_classes[self.actions](self.request.data)
+        if self.action in self.request_serializer_classes:
+            serializer = self.request_serializer_classes[self.action](data=self.request.data)
         else:
-            serializer = self.get_serializer(self.request.data)
+            serializer = self.get_serializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
         return (serializer.validated_data, serializer.context)
