4234053e5635017a7e8e411bbc275b2770be409f
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 13:52:36 +0530

    feat: Organisation Admin

--- passcave/apps/organisation/admin.py
+++ passcave/apps/organisation/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from apps.organisation.models import Organisation
+
+
 # Register your models here.
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ("admin", "org_type")
+
+
+admin.site.register(Organisation, OrganisationAdmin)
