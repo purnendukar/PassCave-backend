f40b93b64f69e6e6175b315cc91f618306ed833c
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-03 13:48:53 +0530

    chore: credential admin

--- passcave/apps/credential/admin.py
+++ passcave/apps/credential/admin.py
@@ -14,9 +14,33 @@ class BaseAdmin(admin.ModelAdmin):
     filter_horizontal = ("access_given",)
 
 
-admin.site.register(BankCard, BaseAdmin)
-admin.site.register(BankDetail, BaseAdmin)
-admin.site.register(WebApplication, BaseAdmin)
-admin.site.register(UPIGateway, BaseAdmin)
-admin.site.register(SecretNote, BaseAdmin)
-admin.site.register(Identity, BaseAdmin)
+class BankCardAdmin(BaseAdmin):
+    list_display = ("owned_by", "card_type", "card_number")
+
+
+class BankDetailAdmin(BaseAdmin):
+    list_display = ("owned_by", "account_number", "bank")
+
+
+class WebApplicationAdmin(BaseAdmin):
+    list_display = ("owned_by", "url", "username", "mobile", "email")
+
+
+class UPIGatewayAdmin(BaseAdmin):
+    list_display = ("owned_by", "portal", "upi_id")
+
+
+class SecretNoteAdmin(BaseAdmin):
+    list_display = ("owned_by", "topic")
+
+
+class IdentityAdmin(BaseAdmin):
+    list_display = ("owned_by", "id_name", "id_number")
+
+
+admin.site.register(BankCard, BankCardAdmin)
+admin.site.register(BankDetail, BankDetailAdmin)
+admin.site.register(WebApplication, WebApplicationAdmin)
+admin.site.register(UPIGateway, UPIGatewayAdmin)
+admin.site.register(SecretNote, SecretNoteAdmin)
+admin.site.register(Identity, IdentityAdmin)
