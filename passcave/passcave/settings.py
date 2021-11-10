ca0d295e960871fd24bb6e69064e391cbe948418
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-11 00:12:05 +0530

    chore: codacy issues

--- passcave/passcave/settings.py
+++ passcave/passcave/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 import environ
 import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-iz6rqwg-!_-yp*wci%wyjsj)%1ndvo@h7@d6vt!j35(w14*d5#"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
