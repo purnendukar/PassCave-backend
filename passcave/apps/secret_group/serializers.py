49b7263a9c043c92b5268de9c55d9b1939a1076b
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-11-10 20:02:17 +0530

    chore: unused import removed

--- passcave/apps/secret_group/serializers.py
+++ passcave/apps/secret_group/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models import fields
 from rest_framework import serializers
 
 from apps.secret_group.models import SecretGroup
--- passcave/apps/user/serializers.py
+++ passcave/apps/user/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models import fields
 from rest_framework import serializers
 
 from rest_framework.authtoken.models import Token
